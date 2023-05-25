8df29160066a0aba4e2679995b2654079a1ffd04
sandeep2630 <katwal.sandeep28@gmail.com>
2023-05-25 18:30:20 +0530

    added reusable funciton for axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,26 @@
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+export const axiosClient = axios.create({
+  baseURL: "http://localhost:5000/api/v1",
   headers: {
     accept: "application/json",
     "Content-Type": "application/json",
   },
   withCredentials: true,
 });
+
+export function getRequest(URL) {
+  return axiosClient.get(`/${URL}`).then((response) => response);
+}
+
+export function postRequest(URL, payload) {
+  return axiosClient.post(`/${URL}`, payload).then((response) => response);
+}
+
+export function patchRequest(URL, payload) {
+  return axiosClient.patch(`/${URL}`, payload).then((response) => response);
+}
+
+export function deleteRequest(URL) {
+  return axiosClient.delete(`/${URL}`).then((response) => response);
+}
