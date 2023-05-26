9cb0a9a2920ebd67a59cc00d490c5cc1136c0862
sandeep2630 <katwal.sandeep28@gmail.com>
2023-05-26 13:29:59 +0530

    added error cathing in axios base function

--- src/api/index.js
+++ src/api/index.js
@@ -10,17 +10,29 @@ export const axiosClient = axios.create({
 });
 
 export function getRequest(URL) {
-  return axiosClient.get(`/${URL}`).then((response) => response);
+  return axiosClient
+    .get(`/${URL}`)
+    .then((response) => response)
+    .catch((response) => response);
 }
 
 export function postRequest(URL, payload) {
-  return axiosClient.post(`/${URL}`, payload).then((response) => response);
+  return axiosClient
+    .post(`/${URL}`, payload)
+    .then((response) => response)
+    .catch((response) => response);
 }
 
 export function patchRequest(URL, payload) {
-  return axiosClient.patch(`/${URL}`, payload).then((response) => response);
+  return axiosClient
+    .patch(`/${URL}`, payload)
+    .then((response) => response)
+    .catch((response) => response);
 }
 
 export function deleteRequest(URL) {
-  return axiosClient.delete(`/${URL}`).then((response) => response);
+  return axiosClient
+    .delete(`/${URL}`)
+    .then((response) => response)
+    .catch((response) => response);
 }
